679171d93ae9741059e584d42b2d134c505e6391
«Elena <svetelenn@gmail.com>
2022-08-28 00:11:20 +0800

    2 homework

---
+++ DZ2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+Console.WriteLine("Input number");
+int a = int.Parse(Console.ReadLine());
+int l = 0;
+if (a < 100)
+{
+    Console.WriteLine("The third digit is missing");
+}
+else
+{
+    while (a > 1000)
+    {
+        a = a / 10;
+    }
+    l = a % 10;
+    Console.WriteLine("Third dijit: "+ l);
+}
+
---
+++ DZ2/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+Console.WriteLine("Input number day");
+int number1 = int.Parse(Console.ReadLine());
+switch (number1)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Not a holiday");
+        break;
+    case 6:
+        Console.WriteLine("Yes, Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Yes, Sunday");
+        break;
+}
