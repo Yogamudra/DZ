fcf6b512e0c441119c8db8b1e5d82f32e2c7515d
«Elena <svetelenn@gmail.com>
2022-09-22 23:37:54 +0800

    5 homework true

---
+++ DZ5/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+Console.Clear();
+int[] a = new int[4];
+Random b = new Random();
+for (int i=0; i <4; i++)
+ {
+ a[i] = b.Next(0, 99);
+ }
+Console.WriteLine(string.Join(",", a));
+int sum = 0;
+for (int i = 0; i < 4; i++)
+    {
+        sum += a[i] % 2 > 0 ? a[i]: 0;
+    }
+Console.WriteLine("В массиве [" + (string.Join(",", a) + "] сумма нечетных чисел -> " + sum));
