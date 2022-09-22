5611faf85e0bc3f7ef9cc00a03483168f58ffb60
«Elena <svetelenn@gmail.com>
2022-09-22 23:35:09 +0800

    5 homework

---
+++ DZ5/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+Console.Clear();
+double n = 0;
+double max = 0;
+double min = 99;
+int[] a = new int[5];
+Random b = new Random();
+for (int i=0; i <5; i++)
+ {
+ a[i] = b.Next(0, 99);
+ }
+Console.WriteLine(string.Join(", ", a));
+for (int i = 0; i < a.Length; i++) 
+{
+if (a[i] <= min) min = a[i];
+if (a[i] > max) max = a[i];
+    n = max - min;
+}
+Console.WriteLine(Math.Round(min, 2) + " -> min");
+Console.WriteLine(Math.Round(max, 2) + " -> max");
+Console.WriteLine(Math.Round(n, 2) + " -> Разница между максимальным и минимальным элементами массива");
