0737f00e53a3e8e13048eadac976934fc96e0396
«Elena <svetelenn@gmail.com>
2022-09-25 00:39:55 +0800

    6 homework

---
+++ DZ6/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4 */
+
+Console.Clear();
+Console.Write("Set number of numbers  ");
+int dimension = int.Parse(Console.ReadLine());
+int[] array_set = new int[dimension];
+for (int i=0; i < dimension; i++)
+ {
+  array_set[i] = int.Parse(Console.ReadLine());
+ }
+Console.WriteLine("Number of positive numbers = " + array_set.Count(x=>x>0));
---
+++ DZ6/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 43: Напишите программу, которая найдёт  точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("Input b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input b2");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input k2");
+double k2 = double.Parse(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+if (k1 == k2)
+{
+    Console.WriteLine(" Straight lines are parallel");
+}
+if (b1 == b2 && k1 == k2)
+{
+    Console.WriteLine(" Direct match");
+}
+else
+{
+    Console.WriteLine($"Points of intersection of two lines ({Math.Round(x, 2)}, {Math.Round(y, 2)})");
+}
